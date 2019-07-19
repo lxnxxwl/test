0a34dad79343dc9a124b916fd6f5e79fcd0774e6
lxnxxwl <lxnxxwl@outlook.com>
2019-07-19 11:09:15 +0800

    eclipse用户2提交更新

--- TestGit/src/com/lxn/Test01.java
+++ TestGit/src/com/lxn/Test01.java
@@ -4,10 +4,11 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("test1");
-		System.out.println("eclipse更改内容");
-		System.out.println("eclipse更改内容");
-		System.out.println("eclipse第二次更改内容");
-		System.out.println("git上更改内容");
+		System.out.println("eclipse鏇存敼鍐呭");
+		System.out.println("eclipse鏇存敼鍐呭");
+		System.out.println("eclipse绗簩娆℃洿鏀瑰唴瀹�");
+		System.out.println("git涓婃洿鏀瑰唴瀹�");
+		System.out.println("eclipse 用户2加入更新");
 	}
 
 }
