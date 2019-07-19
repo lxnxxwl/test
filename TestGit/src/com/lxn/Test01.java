e6ca73f4ba360836d18c03f7341560cbaafa0a9c
lxnxxwl <lxnxxwl@outlook.com>
2019-07-19 15:34:19 +0800

    x

--- TestGit/src/com/lxn/Test01.java
+++ TestGit/src/com/lxn/Test01.java
@@ -4,14 +4,15 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("test1");
-		System.out.println("eclipse鏇存敼鍐呭");
-		System.out.println("eclipse鏇存敼鍐呭");
-		System.out.println("eclipse绗簩娆℃洿鏀瑰唴瀹�");
-		System.out.println("git涓婃洿鏀瑰唴瀹�");
-		System.out.println("eclipse 用户2加入更新");
+		System.out.println("eclipse閺囧瓨鏁奸崘鍛啇");
+		System.out.println("eclipse閺囧瓨鏁奸崘鍛啇");
+		System.out.println("eclipse缁楊兛绨╁▎鈩冩纯閺�鐟板敶鐎癸拷");
+		System.out.println("git娑撳﹥娲块弨鐟板敶鐎癸拷");
+		System.out.println("eclipse 鐢ㄦ埛2鍔犲叆鏇存柊");
 		/**
-		 * System.out.println("eclipse 用户2加入更新");
+		 * System.out.println("eclipse 鐢ㄦ埛2鍔犲叆鏇存柊");
 		 * */
+		//x
 	}
 
 }
